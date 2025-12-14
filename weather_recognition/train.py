484b9e672b465fa23a298c4c607d3a1d0cc5afa8
Дмитрий Ткачёв <dmvltkachev@edu.hse.ru>
2025-12-14 17:10:01 +0300

    Fix config

--- weather_recognition/train.py
+++ weather_recognition/train.py
@@ -22,7 +22,7 @@ def get_git_commit_id() -> str:
         return "unknown"
 
 
-@hydra.main(config_path="./conf", config_name="config", version_base=None)
+@hydra.main(config_path="../conf", config_name="config", version_base=None)
 def main(cfg: DictConfig):
     pull_data()
     print(OmegaConf.to_yaml(cfg))
